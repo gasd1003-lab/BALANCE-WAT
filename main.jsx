fdea9f00b9148bc0db6eef7b780b0e8c63aaeedc
gasd1003-lab <gasd1003@gmail.com>
2026-04-12 21:39:36 -0500

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './app_work_and_travel_offline.jsx' // Asegúrate de que el nombre coincida con tu archivo
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
